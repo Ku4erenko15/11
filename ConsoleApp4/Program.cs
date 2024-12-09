5c7bd78ed4c769e8f60418a6d6f3c721f11cd908
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2024-12-09 20:47:02 +0200

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 4.Сортировка массива по возрастанию.
+            int[] omas = { 2, 3, 7, 8, 2, 3, 4, 6, 9, 5 };
+            Array.Sort(omas);
+            foreach (var om in omas)
+            {
+                Console.Write(om + " ");
+            }
+            Array.Reverse(omas); 
+            Console.WriteLine();
+            foreach (var om in omas)
+            {
+                Console.Write(om + " ");
+            }
+            Console.ReadKey ();
+        }
+    }
+}
