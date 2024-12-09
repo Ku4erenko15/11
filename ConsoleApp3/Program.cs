3caaf92cc726e334dd7ae751493e28ab2897f495
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2024-12-09 20:35:14 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 3.Найти максимальное и минимальное значение в массиве.
+            int[]omas = { 1, 2, 3, 4 , 5 };
+            int max = omas.Max();
+            int min = omas.Min();
+            Console.WriteLine(max, min);
+            Console.ReadKey();
+        }
+    }
+}
