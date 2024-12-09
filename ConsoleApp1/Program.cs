bf1bfc7532eb15b6f5395e696e57607f5d8a8a5a
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2024-12-09 19:35:58 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1.Определить количество элементов одномерного массива
+            int[] omas = { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine($"Size:{omas.Length}");
+            string[] omas1 = { "Iphone", "Samsung", "Google" };
+            int size = omas1.Length;
+            Console.WriteLine($"Size:{size}");
+            Console.ReadKey();
+        }
+    }
+}
