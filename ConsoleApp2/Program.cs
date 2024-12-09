35b00afc0086b996e10587672b26c56cde12c89c
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2024-12-09 20:24:31 +0200

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 2.Обнулить заданный диапазон элементов в одномерном массиве.
+            int[] omas = Input(Size());
+            Array.Clear(omas,1,3);
+            Output(omas);
+            Console.ReadKey(); 
+        }
+        static int Size()
+
+        {
+            Console.WriteLine("Size");
+            int size;
+            while (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+            { Console.WriteLine("error"); }
+            return size;
+        }
+        static int[] Input (int size)
+        {
+            int[] omas = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"Input {i+1}");
+                omas[i]=int.Parse(Console.ReadLine());
+            }
+            return omas;
+        }
+        static void Output(int[]omas)
+        {
+            foreach (var el in omas)
+            {
+                Console.WriteLine(el);
+            }
+        }
+    }
+}
