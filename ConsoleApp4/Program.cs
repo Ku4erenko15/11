3f3aa6f50e8f23e6e7754cbda2c3a8503408fcbd
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2024-12-10 12:15:36 +0300

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 10.Удаление всех четных элементов из массива. В новом массиве сложить элементы
+            int[] omas = {3,7,1,9,5,6,8,10};
+            omas =omas.Where(x => x%2!=0).ToArray();
+            foreach (int i in omas)
+            {
+                Console.WriteLine(i);              
+            }
+            int sum = omas.Sum();
+            Console.WriteLine(sum);
+            Console.ReadKey();  
+        }
+    }
+}
