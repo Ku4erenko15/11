ca38e0cbd103320e0f7f4086bbdd87daf355c3f8
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2024-12-10 11:58:43 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 8.Изменение размера массива.
+            int[]omas = {8,4,5,4,6,3,2,1};
+            Array.Resize(ref omas, 4);
+            foreach (var num in omas) 
+            {
+                Console.Write($"{num}"); 
+            }   
+            Console.ReadLine();
+        }
+    }
+}
