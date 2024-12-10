6a5f40f5ada9cc14f64f5e3d12ffee1c603db4f3
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2024-12-10 11:52:30 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 7.Поиск элемента по условию.
+            string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+            string first = Array.Find(people, person => person.Length > 3);
+            Console.WriteLine(first);
+            string last = Array.FindLast(people, person => person.Length > 3);
+            Console.WriteLine(last);
+            string[] group = Array.FindAll(people, person => person.Length == 3);
+            foreach (var pers in group)
+            { 
+                Console.WriteLine(pers);
+            }
+            Console.ReadLine();
+        }
+    }
+}
